e9fb2ed4173ffb67ac650b4f1aafbac9486cbcc7
Name <alecli2014@gmail.com>
2020-02-19 02:28:57 +0000

    made no progress

---
+++ js/script.js
@@ -0,0 +1,25 @@
+let songList = [
+    "something1",
+    "something2",
+    "something3"
+];
+let Artist = [
+    "someone1",
+    "someone2",
+    "someone3"
+];
+let songLength = [
+    "length1",
+    "length2",
+    "length3"
+];
+let image = [
+    "image1",
+    "image2",
+    "image3"
+];
+let link = [
+    "link3",
+    "link2",
+    "link3"
+];
